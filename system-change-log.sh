60e1296b1545ff840f79fa52b28fb0844c80fc43
Jonathan Wilson <jonwilami@gmail.com>
2023-07-04 15:49:38 -0400

    Adding more files

---
+++ system-change-log.sh
@@ -0,0 +1,7 @@
+# Internet keeps going down
+sudo apt-get purge network-manager
+sudo apt-get update
+sudo apt-get autoremove
+sudo apt-get upgrade
+sudo apt-get install network-manager
+sudo apt install ubuntu-desktop
